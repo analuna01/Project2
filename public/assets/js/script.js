97cf7b41a6fa3043a547db5e82783738fe017a24
Aldo Carrillo <aldo.carrillo22@gmail.com>
2020-10-06 21:27:20 -0700

    html grid

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,11 @@
-$(function() {
-    
-    
-
+$.ajax({
+  url: "https://ws.adanta.mx/api/animals/ById/1",
+  type: "GET",
+  dataType: "json",
+  success: function (animal) {
+    console.log(animal);
+    var image = document.getElementById("img1");
+    image.setAttribute("src", animal.url);
+    image.setAttribute("style", "width: 256px;");
+  },
 });
