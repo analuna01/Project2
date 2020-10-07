333ff12fa114071fcd975fc670f4ddbf4f8a9964
Elliott Jones <jones.elliott@outlook.com>
2020-10-06 21:28:53 -0700

    Added form to modal

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3317,
+  port: 3306,
   user: "root",
   password: "root",
   database: "memory_db",
