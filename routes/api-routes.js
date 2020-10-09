19a889743b49ecc3c1877d2413469d4233e1b00c
Aldo Carrillo <aldo.carrillo22@gmail.com>
2020-10-08 21:20:53 -0700

    add route gameboard

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,34 +1,22 @@
 var db = require("../models");
 
 module.exports = function (app) {
-
-<<<<<<< HEAD
-    // create a user
-    app.post('/api/user', (req, res) => {
-        db.user.create(req.body)
-            .then(user => res.json(user))
-    })
-    // get all users
-    app.get('/api/user', (req, res) => {
-        db.user.findAll().then(users => res.json(users))
-    })
-=======
-    //returns the board table
-    app.get("/api/board", function (req, res) {
-        db.board.findAll({}).then(function (results) {
-            res.json(results);
-        });
+  //returns the board table
+  app.get("/api/board", function (req, res) {
+    db.board.findAll({}).then(function (results) {
+      res.json(results);
     });
+  });
 
-    //empties current board
-    app.put("/api/reset", function (req, res) {
-        db.board.destroy({
-            truncate: true,
-            cascade: false
-        }).then(function (results) {
-            res.json(results);
-        })
-    })
-
->>>>>>> 21426175094aaf5711fb958085dd180e39e3f0c7
+  //empties current board
+  app.put("/api/reset", function (req, res) {
+    db.board
+      .destroy({
+        truncate: true,
+        cascade: false,
+      })
+      .then(function (results) {
+        res.json(results);
+      });
+  });
 };
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,21 +12,11 @@ module.exports = function (app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-<<<<<<< HEAD
   app.get("/", function (req, res) {
     res.render("index");
-    // res.sendFile(path.join(__dirname, "../views/index.handlebars"));
-=======
-  app.get("/", function(req, res) {
-      res.render("index");
->>>>>>> aba3278cd1fd8c793355f2fc4a06a095a4d60068
   });
 
   app.get("/gameboard", function (req, res) {
-    res.render("gameboard");
-  });
-
-  app.get("/gametest", function (req, res) {
     var cardImages = [
       "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
       "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
