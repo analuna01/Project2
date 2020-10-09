510e7fbd0ea423f612370de66d7d1bac975bc8a3
Aldo Carrillo <aldo.carrillo22@gmail.com>
2020-10-08 21:25:14 -0700

    add route gameboard

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,40 +1,36 @@
 var db = require("../models");
-
 module.exports = function (app) {
-
-    //returns the board table
-    app.get("/api/board", function (req, res) {
-        db.board.findAll({}).then(function (results) {
-            res.json(results);
-        });
+  //returns the board table
+  app.get("/api/board", function (req, res) {
+    db.board.findAll({}).then(function (results) {
+      res.json(results);
     });
   });
-
-    //empties current board
-    app.put("/api/reset", function (req, res) {
-        db.board.destroy({
-            truncate: true,
-            cascade: false
-        }).then(function (results) {
-            res.json(results);
-        })
-    })
-
-    // create a user
-    app.post('/api/user', (req, res) => {
-        db.user.create({
-            email: req.body.email,
-            username: req.body.username,
-            password: req.body.password
-          })
-            .then(function(results) {
-              res.json(results);
-            });
-      
-    })
-    // get all users
-    app.get('/api/user', (req, res) => {
-        db.user.findAll().then(users => res.json(users))
-    })
-
+  //empties current board
+  app.put("/api/reset", function (req, res) {
+    db.board
+      .destroy({
+        truncate: true,
+        cascade: false,
+      })
+      .then(function (results) {
+        res.json(results);
+      });
+  });
+  // create a user
+  app.post("/api/user", (req, res) => {
+    db.user
+      .create({
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+      })
+      .then(function (results) {
+        res.json(results);
+      });
+  });
+  // get all users
+  app.get("/api/user", (req, res) => {
+    db.user.findAll().then((users) => res.json(users));
+  });
 };
