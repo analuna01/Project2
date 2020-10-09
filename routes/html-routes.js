fe8f51340b1ec33e98e02ff538b0e5423fe039b3
Aldo Carrillo <aldo.carrillo22@gmail.com>
2020-10-08 21:09:33 -0700

    add array images to handlebar

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,18 +8,39 @@ var path = require("path");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
-
+module.exports = function (app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
-      res.render("index");
+  app.get("/", function (req, res) {
+    res.render("index");
     // res.sendFile(path.join(__dirname, "../views/index.handlebars"));
   });
 
-  app.get("/gameboard", function(req, res) {
+  app.get("/gameboard", function (req, res) {
     res.render("gameboard");
   });
 
+  app.get("/gametest", function (req, res) {
+    var cardImages = [
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
+    ];
+
+    res.render("board", { cards: cardImages });
+  });
 };
