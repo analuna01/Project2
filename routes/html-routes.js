90712b0a158aea8b4ec445fe58d02ae8e9d4476f
patches9119 <pbsanders1@gmail.com>
2020-10-08 23:28:27 -0700

    fogot to get rid of some arrows on a merge

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,13 +12,8 @@ module.exports = function (app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-<<<<<<< HEAD
-  app.get("/", function(req, res) {
-      res.render("index");
-=======
   app.get("/", function (req, res) {
     res.render("index");
->>>>>>> 510e7fbd0ea423f612370de66d7d1bac975bc8a3
   });
 
   app.get("/gameboard", function (req, res) {
@@ -41,6 +36,8 @@ module.exports = function (app) {
       "https://www.flaticon.com/svg/static/icons/svg/2281/2281113.svg",
     ];
 
-    res.render("board", { cards: cardImages });
+    res.render("board", {
+      cards: cardImages
+    });
   });
 };
