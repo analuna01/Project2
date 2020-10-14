5313a22aebfa3bd0c50bb4e406965d63f61efd84
patches9119 <pbsanders1@gmail.com>
2020-10-13 20:42:02 -0700

    installed helmet

--- config/connection.js
+++ config/connection.js
@@ -7,26 +7,14 @@ var SQLPORT = require(process.env.SQLPORT);
 require("dotenv").config();
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-<<<<<<< HEAD
-var sequelize = new Sequelize("memory_db", "root", "root", {
-  host: "localhost",
-  port: "3318",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000,
-  },
-});
-=======
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: "3000",
+    port: "3318",
     user: "root",
-    password: "marley28",
+    password: "root",
     database: "memory_db",
   })
   // var sequelize = new Sequelize("memory_db", "root", "root", {
@@ -39,7 +27,6 @@ if (process.env.JAWSDB_URL){
   //     idle: 10000,
 
 };
->>>>>>> acd153dfc51671bf12d44b69a233c43e1af31bca
 
 // Exports the connection for other files to use
 module.exports = connection;
